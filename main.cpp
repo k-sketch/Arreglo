c6ee3abf049692adb2dce2a5fc348568473f6ab5
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2019-09-25 09:52:55 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+///1. Imprimir los elementos
+int main(){
+    int arr[3]={1,2,3};
+    int lista={};
+    for(int i=0;i<3;i++)
+        lista=arr[i];
+    cout << lista;
+}
+///2. Suma (recursiva e iteractiva)
+int main(){
+    int suma=0;
+    int arr[3]={1,2,3};
+    for(int i=0;i<3;i++)
+        suma+=arr[i];
+    cout << suma;
+    return 0;
+}
+
+
+int sumarecursiva(int tam){
+
+}
+
+
+///3. Invertir (recursiva e iteractiva)
+int main(){
+    int arr[3]={1,2,3};
+    int x
+}
+
+///4. Ordenamiento (burbuja,insertion,quicksort)
